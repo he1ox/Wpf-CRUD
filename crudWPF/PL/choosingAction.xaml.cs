71de72111a88f77970702e7f831bc60ea9edd821
Jorge Lopez <jlopezg112@miumg.edu.gt>
2021-05-19 21:08:46 -0600

    Ventana Eleccion de Accion
    Se crea la ventana para elegir una accion, ya sea entre proveedores o
    productos.

---
+++ crudWPF/PL/choosingAction.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace crudWPF.PL
+{
+    /// <summary>
+    /// Lógica de interacción para choosingAction.xaml
+    /// </summary>
+    public partial class choosingAction : Window
+    {
+        public choosingAction()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCerrar(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
